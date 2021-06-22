2a628347234cea80947b52c415639175000ba6b8
Chukwuma Okoroji <dandychux@gmail.com>
2021-06-22 19:25:10 -0400

    Update
    Updates:
    - Added style
    - Added validation script
    - Built the actual form

---
+++ app.js
@@ -0,0 +1,39 @@
+/* form validation */
+
+var exclamation = document.getElementsByClassName('error');
+var form = document.getElementsById('form');
+var firstName = document.getElementById('firstName');
+var lastName = document.getElementById('lastName');
+var email = document.getElementbyId('email');
+var password = document.getElementById('password');
+var formError = document.getElementsByClassName('form_error');
+var input = document.getElementsByTagName('input');
+
+/* validation */
+function formValidation(event) {
+    /* for loop for all input fields except submit input, so it is 4 */
+    for (i=0; i<4; i++) {
+        /* clear the form */
+        input[i].classList.remove('error');
+        formError[i].style.display = 'none';
+
+        /* email validation */
+        if (!email.validity.valid) {
+            formError[2].style.display = 'block';
+            email.classList.add('error');
+            event.preventDefault();
+        }
+
+        /* validatioh of other fields */
+        if((input.type != 'email' || input.type != 'submit') && input[i].value=='') {
+            // using css in js
+            formError[i].style.display = 'block';
+            input[i].classList.add('error');
+            event.preventDefault();
+
+        }
+    }
+}
+
+/* event listener on button, call the formValidation function*/
+form.addEventListener('submit', formValidation, false)
